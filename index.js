d9b5b3f2772ce0dafc6a70e096cc0ba5d92d3204
Heather Arbuckle <bucklehead@gmail.com>
2019-05-15 22:05:44 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,22 @@
+var recipes = {};
+
+function updateObjectWIthKeyAndValue(object, key, value) {
+  var newRecipes = Object.assign({}, object, {key: value});
+  return newRecipes;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  delete key.value;
+  return recipes;
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newRecipes = Object.assign({}, object);
+  delete newRecipes.key;
+  return newRecipes;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete.object[key];
+  return recipes;
+}
