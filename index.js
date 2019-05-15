64b5b5973bf326ff3ab83b4f610b72bb8a62cff2
Heather Arbuckle <bucklehead@gmail.com>
2019-05-15 22:07:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ var recipes = {};
 //   var newRecipes = Object.assign({}, object, {key: value});
 //   return newRecipes;
 // }
-// 
+//
 // function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 //   delete key.value;
 //   return recipes;
 // }
-// 
+//
 // function deleteFromObjectByKey(object, key) {
 //   var newRecipes = Object.assign({}, object);
 //   delete newRecipes.key;
 //   return newRecipes;
 // }
-// 
+//
 // function destructivelyDeleteFromObjectByKey(object, key) {
 //   delete.object[key];
 //   return recipes;
